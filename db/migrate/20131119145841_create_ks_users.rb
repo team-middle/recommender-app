06b805d859ab5e60f61535df2517adff21b3587a
jnoconor <joe.oconor@gmail.com>
2013-11-19 10:34:55 -0500

    add ksusers, ksprojecs, and backer join tables

---
+++ db/migrate/20131119145841_create_ks_users.rb
@@ -0,0 +1,6 @@
+class CreateKsUsers < ActiveRecord::Migration
+  def change
+    create_table :ks_users do |t|
+    end
+  end
+end
---
+++ db/migrate/20131119151049_create_ks_projects.rb
@@ -0,0 +1,6 @@
+class CreateKsProjects < ActiveRecord::Migration
+  def change
+    create_table :ks_projects do |t|
+    end
+  end
+end
