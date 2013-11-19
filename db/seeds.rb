b6766e21b419dc0f5cb408f4b835a361a31e6252
jnoconor <joe.oconor@gmail.com>
2013-11-19 15:59:26 -0500

    add dumpfile of current state of db

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,29 +30,56 @@ urls_for_backed_projects = user_page.css("a.project_item").collect do |project|
   puts "scraped #{ks_project.url}"
 end
 
-project = KsProject.first
-project_backers_page = Nokogiri::HTML(open("http://www.kickstarter.com/#{project.url}/backers"))
-project_backers_page.css(".NS_backers__backing_row").each do |row|
-  user_url = row.css("a").attr("href").value
-  project.ks_users.find_or_create_by(:url => user_url)
-  puts "scraped #{user_url}"
-end
-project.title = project_backers_page.xpath("//meta[@property='og:title']/@content").text
-project.creator_id = project_backers_page.xpath("//meta[@property='kickstarter:creator']/@content").text.split(/\//).last
-project.description = project_backers_page.xpath("//meta[@property='og:description']/@content").text
-project.parent_category = project_backers_page.xpath("//li['@class=category']/@data-project-parent-category").text
-project.category = project_backers_page.css("div.NS-project_-running_board li.category").text.strip
-project.funding_goal = project_backers_page.xpath("//div['@id=pledged']/@data-goal").text.to_i
-project.pledged = project_backers_page.xpath("//div['@id=pledged']/@data-pledged").text.to_i
-project.latitude = project_backers_page.xpath("//meta[@property='kickstarter:location:latitude']/@content").text
-project.longitude = project_backers_page.xpath("//meta[@property='kickstarter:location:longitude']/@content").text
-project.updates_count = project_backers_page.xpath("//span['@id=updates_count']/@data-updates-count").text.to_i
-project.comments_count = project_backers_page.xpath("//span['@id=comments_count']/@data-comments-count").text.to_i
-project.backer_count = project_backers_page.xpath("//data['@itemprop=Project[backers_count]']/@data-value").first.value.to_i
+projects = KsProject.all
+projects.each do |project|
+  project_backers_page = Nokogiri::HTML(open("http://www.kickstarter.com/#{project.url}/backers"))
+  project_backers_page.css(".NS_backers__backing_row").each do |row|
+    user_url = row.css("a").attr("href").value
+    project.ks_users.find_or_create_by(:url => user_url)
+    puts "scraped #{user_url}"
+  end
+  if !project.scraped
+    project.title = project_backers_page.xpath("//meta[@property='og:title']/@content").text
+    project.creator_id = project_backers_page.xpath("//meta[@property='kickstarter:creator']/@content").text.split(/\//).last
+    project.description = project_backers_page.xpath("//meta[@property='og:description']/@content").text
+    project.parent_category = project_backers_page.xpath("//li['@class=category']/@data-project-parent-category").text
+    project.category = project_backers_page.css("div.NS-project_-running_board li.category").text.strip
+    project.funding_goal = project_backers_page.xpath("//div['@id=pledged']/@data-goal").text.to_i
+    project.pledged = project_backers_page.xpath("//div['@id=pledged']/@data-pledged").text.to_i
+    project.latitude = project_backers_page.xpath("//meta[@property='kickstarter:location:latitude']/@content").text
+    project.longitude = project_backers_page.xpath("//meta[@property='kickstarter:location:longitude']/@content").text
+    project.updates_count = project_backers_page.xpath("//span['@id=updates_count']/@data-updates-count").text.to_i
+    project.comments_count = project_backers_page.xpath("//span['@id=comments_count']/@data-comments-count").text.to_i
+    project.backer_count = project_backers_page.xpath("//data['@itemprop=Project[backers_count]']/@data-value").first.value.to_i
+  end
 
-
+  if project.save
+    project.scraped = true 
+    project.save 
+  else
+    puts "there was an error"
+  end
 puts "   finished scraping #{project.title}"
-binding.pry
+end
+
+users = KsUser.all
+users.each do |user|
+  url = user.url
+  user_page = Nokogiri::HTML(open("http://www.kickstarter.com/#{url}"))
+
+  # coffee.css(".NS_backers__backing_row").last.css("a").attr("href").value
+  user = KsUser.find_or_create_by(:url => url)
+
+  urls_for_backed_projects = user_page.css("a.project_item").collect do |project|
+    project_url = project.attr('href')
+    ks_project = KsProject.find_or_create_by(:url => project_url)
+    user.ks_project_backers.find_or_create_by(:ks_project => ks_project)
+    puts "scraped #{ks_project.url}"
+  end
+  user.scraped = true 
+  user.save
+  puts "    finished scraping #{user.url}"
+end
 
 
 10.times do 
