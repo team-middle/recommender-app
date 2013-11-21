f5dbb332791850b2d6a979432de83b91e417bd71
Michael Polycarpou <michaelpolycarpou@gmail.com>
2013-11-21 16:25:05 -0500

    broke everything in the algorithm

--- app/models/kmeans.rb
+++ app/models/kmeans.rb
@@ -2,24 +2,19 @@ class Kmeans
   attr_accessor :map, :points, :assignments, :centers
 
   def calculate_new_centers(k)
-    self.centers.map do |group|
-      group_arr = assignments.select do |assignment|
-        assignment[1] == group
-      end
-      sum = []
+    dimension = centers.first.size
+    self.centers.map do |center|
+      cluster = assignments[center]
       mean = []
-      (0..group.count-1).each do |d|  
-        sum[d] = 0
-      end
-      group_arr.each do |point|
-        (0..group.count-1).each do |d|
-          sum[d] += point[0][d]
+      sum = Array.new(dimension,0)
+      cluster.each do |point|
+        dimension.times do |d| #d stands for dimension
+          sum[d] += point[d]
         end
       end
-      if group_arr.count != 0
-        (0..group.count-1).each do |d|
-          mean[d] = sum[d]/group_arr.count
-        end
+      dimension.times do |d|
+        binding.pry
+        mean[d] = sum[d]/cluster.count
       end
       mean
     end
@@ -44,41 +39,36 @@ class Kmeans
   end
 
   def reassign_groups(centers)
-    assignments.each do |assignment|
-      point = assignment[0]
-      new_group = centers.min_by do |center|
+    new_assignment = Hash.new {|hash, key| hash[key] = []}
+    assignments.values.flatten(1).each do |point|
+      new_cluster = centers.min_by do |center|
         distance(center,point)
       end
-      assignment[1] = new_group
+      new_assignment[new_cluster] << point
     end
+    self.assignments = new_assignment
    #old_assignments.sort != assignments.sort #changed?
   end
 
 
   def cluster(k,points,max_iters)
-    self.assignments = []
-    self.centers = points.shuffle.take(k)
-    points.each do |point|
-      self.assignments << [point, centers.sample]
+    self.assignments = {}
+    points.uniq.shuffle.take(k).each do |center|
+      self.assignments[center] = []
     end
-  
+    points.each do |point|
+      self.assignments[assignments.keys.sample] << point
+    end
+    self.centers = assignments.keys
     iter = 0
     begin 
       self.centers = calculate_new_centers(k)
       reassign_groups(centers)
       iter += 1
     end  while iter <= max_iters
-
-    clean_hash = Hash[*assignments.flatten(1)]
-    inverse = Hash.new() { |hash, key| hash[key] = []; }
-    clean_hash.each_pair() { |key, value| inverse[value].push(key); }
-    inverse
+    self.assignments
+    binding.pry
   end # end of cluster method
-
-  # def centers_to_database
-  #   self.centers.each_with_index do |center, i|
-  #     Center.add_center(center[i])
-  # end
 
 
 end # end of class
