a30874756c3cb6d31b655c7e1ec4f02eb8146262
jnoconor <joe.oconor@gmail.com>
2013-11-24 15:20:58 -0500

    convert db to postgres, add uniq validation to ks_users/ks_projects on their has_many through associations (this will prevent any duplicate associations being registered when we do category counting), add tests for scrape method (in attempt to prevent duplicate records from being created)

--- app/models/ks_project.rb
+++ app/models/ks_project.rb
@@ -1,7 +1,8 @@
 class KsProject < ActiveRecord::Base
-  has_many :ks_project_backers
-  has_many :ks_users, :through => :ks_project_backers
+  has_many :ks_project_backers, :dependent => :destroy
+  has_many :ks_users, -> { uniq }, :through => :ks_project_backers
   has_many :recommendations
+
 
   def still_active?
     begin
@@ -20,6 +21,87 @@ class KsProject < ActiveRecord::Base
 
   def ending_soon?
 
+  end
+
+  def add_user(user)
+    # if !KsProjectBacker.where(:ks_project => self).where(:ks_user => user).empty?
+    #   binding.pry
+    #   return
+    # else
+      self.ks_project_backers.find_or_create_by(:ks_user => user)
+      self.save
+    # end
+  end
+
+  def self.scrape_without_users(projects)
+    projects.each do |project|
+      if project.scraped
+        puts "already scraped #{project.url}"
+        next
+      else
+        project.title = project_backers_page.xpath("//meta[@property='og:title']/@content").text
+        project.creator_id = project_backers_page.xpath("//meta[@property='kickstarter:creator']/@content").text.split(/\//).last
+        project.description = project_backers_page.xpath("//meta[@property='og:description']/@content").text
+        project.parent_category = project_backers_page.xpath("//li['@class=category']/@data-project-parent-category").text
+        project.category = project_backers_page.css("div.NS-project_-running_board li.category").text.strip
+        project.funding_goal = project_backers_page.xpath("//div['@id=pledged']/@data-goal").text.to_i
+        project.pledged = project_backers_page.xpath("//div['@id=pledged']/@data-pledged").text.to_i
+        project.latitude = project_backers_page.xpath("//meta[@property='kickstarter:location:latitude']/@content").text
+        project.longitude = project_backers_page.xpath("//meta[@property='kickstarter:location:longitude']/@content").text
+        project.end_date = project_backers_page.xpath("//h5 ['@class=poll']/@data-end_time").text
+        project.updates_count = project_backers_page.xpath("//span['@id=updates_count']/@data-updates-count").text.to_i
+        project.comments_count = project_backers_page.xpath("//span['@id=comments_count']/@data-comments-count").text.to_i
+        project.backer_count = project_backers_page.xpath("//data['@itemprop=Project[backers_count]']/@data-value").first.value.to_i
+        project.image_url = project_backers_page.xpath("//meta[@property='og:image']/@content").text
+      end
+
+      if project.save
+        project.scraped = true 
+        project.save 
+      else
+        puts "there was an error"
+      end
+    puts "   finished scraping #{project.title}, #{project.id}"
+    end
+  end
+
+  def self.scrape(projects)
+    projects.each do |project|
+      if project.scraped
+        puts "already scraped #{project.url}"
+        next
+      else
+        project_backers_page = Nokogiri::HTML(open("http://www.kickstarter.com/#{project.url}/backers"))
+        project_backers_page.css(".NS_backers__backing_row").each do |row|
+          user_url = row.css("a").attr("href").value
+          user = KsUser.find_or_create_by(:url => user_url)
+          project.add_user(user)
+          puts "scraped #{user_url}"
+        end
+        project.title = project_backers_page.xpath("//meta[@property='og:title']/@content").text
+        project.creator_id = project_backers_page.xpath("//meta[@property='kickstarter:creator']/@content").text.split(/\//).last
+        project.description = project_backers_page.xpath("//meta[@property='og:description']/@content").text
+        project.parent_category = project_backers_page.xpath("//li['@class=category']/@data-project-parent-category").text
+        project.category = project_backers_page.css("div.NS-project_-running_board li.category").text.strip
+        project.funding_goal = project_backers_page.xpath("//div['@id=pledged']/@data-goal").text.to_i
+        project.pledged = project_backers_page.xpath("//div['@id=pledged']/@data-pledged").text.to_i
+        project.latitude = project_backers_page.xpath("//meta[@property='kickstarter:location:latitude']/@content").text
+        project.longitude = project_backers_page.xpath("//meta[@property='kickstarter:location:longitude']/@content").text
+        project.end_date = project_backers_page.xpath("//h5 ['@class=poll']/@data-end_time").text
+        project.updates_count = project_backers_page.xpath("//span['@id=updates_count']/@data-updates-count").text.to_i
+        project.comments_count = project_backers_page.xpath("//span['@id=comments_count']/@data-comments-count").text.to_i
+        project.backer_count = project_backers_page.xpath("//data['@itemprop=Project[backers_count]']/@data-value").first.value.to_i
+        project.image_url = project_backers_page.xpath("//meta[@property='og:image']/@content").text
+      end
+
+      if project.save
+        project.scraped = true 
+        project.save 
+      else
+        puts "there was an error"
+      end
+    puts "   finished scraping #{project.title}, #{project.id}"
+    end
   end
 
 end
