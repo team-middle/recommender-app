b781cf49f44823d77a6e28fa1ab4acbc5c0434a4
jnoconor <joe.oconor@gmail.com>
2013-11-25 13:52:14 -0500

    get ashley styles partially working

---
+++ lib/dictionary_with_subs.rb
@@ -0,0 +1,275 @@
+class Dictionary 
+CATEGORY_DICTIONARY = { # film
+    animation
+  documentary
+  narrative film 
+  short film
+  webseries
+:"actor/director" => "film",
+:"movie" => "film",
+:"producer" => "film",
+:"writer" => "film",
+:"studio" => "film",
+:"movie theater" => "film",
+:"tv/movie award" => "film",
+:"fictional character" => "film",
+:"tv show" => "film",
+:"tv network" => "film",
+:"tv channel" => "film",
+:"tv genre" => "film",
+:"movies/music" => "film",
+# music
+  classical music
+  country & folk 
+  electronic music
+  hip hop 
+  indie rock 
+  jazz
+  metal
+  pop 
+  rock
+  world music 
+:"album" => "music",
+:"song" => "music",
+:"musician/band" => "music",
+:"musical instrument" => "music",
+:"playlist" => "music",
+:"music video" => "music",
+:"concert tour" => "music",
+:"concert venue" => "music",
+:"radio station" => "music",
+:"record label" => "music",
+:"music award" => "music",
+:"music chart" => "music",
+# publishing/comics
+  art book
+  children's book
+  fiction
+  journalism 
+  nonfiction
+  perodical
+  poetry
+  radio & podcast'
+:"book" => "publishing",
+:"author" => "publishing",
+:"book store" => "publishing",
+:"library" => "publishing",
+:"magazine" => "publishing",
+:"editor" => "publishing",
+:"journalist" => "publishing",
+:"event planning/event services" => "publishing",
+:"media/news/publishing" => "publishing",
+:"professional services" => "publishing",
+:"school" => "publishing",
+:"non-profit organization" => "publishing",
+:"government organization" => "publishing",
+:"cause" => "publishing",
+:"political party" => "publishing",
+:"education" => "publishing",
+:"legal/law" => "publishing",
+:"news personality" => "publishing",
+:"teacher" => "publishing",
+:"non-governmental organization (ngo)" => "publishing",
+:"community organization" => "publishing",
+:"political organization" => "publishing",
+# art
+  conceptual art
+  crafts
+  digital art
+  illustration
+  painting
+  performance art
+  mixed media
+  public art 
+  sculpture
+:"attractions/things to do" => "art",
+:"museum/art gallery" => "art",
+:"artist" => "art",
+:"tours/sightseeing" => "art",
+:"monarch" => "art",
+:"landmark" => "art",
+# food
+:"restaurant/cafe" => "food",
+:"chef" => "food",
+:"bar" => "food",
+:"club" => "food",
+:"wine/spirits" => "food",
+:"food/beverages" => "food",
+:"food/grocery" => "food",
+# dance
+:"dancer" => "dance",
+# technology
+  hardware
+:"website" => "technology",
+:"cars" => "technology",
+  hardware
+:"computers" => "technology",
+:"software" => "technology",
+  open software
+:"office supplies" => "technology",
+  hardware
+:"electronics" => "technology",
+  hardware
+:"appliances" => "technology",
+  hardware
+:"building materials" => "technology",
+  hardware
+:"commercial equipment" => "technology",
+  hardware
+:"tools/equipment" => "technology",
+  hardware
+:"outdoor gear/sporting goods" => "technology",
+  hardware
+:"energy/utility" => "technology",
+  hardware
+:"automobiles and parts" => "technology",
+  hardware
+:"aerospace/defense" => "technology",
+  hardware
+:"mining/materials" => "technology",
+  hardware
+:"farming/agriculture" => "technology",
+  hardware
+:"chemicals" => "technology",
+  hardware
+:"engineering/construction" => "technology",
+  hardware
+:"telecommunication" => "technology",
+:"biotechnology" => "technology",
+:"computers/technology" => "technology",
+  open software
+:"internet/software" => "technology",
+  open software
+:"automotive" => "technology",
+  hardware
+:"bank/financial services" => "technology",
+:"health/medical/pharmacy" => "technology",
+:"transportation" => "technology",
+  hardware
+:"health/medical/pharmaceuticals" => "technology",
+:"industrials" => "technology",
+  hardware
+:"transport/freight" => "technology",
+:"vitamins/supplements" =>"technology" ,
+# design
+  graphic design
+  produt design
+:"bags/luggage" => "design",
+:"home decor" => "design",
+:"furniture" => "design",
+:"household supplies" => "design",
+:"kitchen/cooking" => "design",
+:"patio/garden" => "design",
+:"home improvement" => "design",
+:"retail and consumer merchandise" => "design",
+:"small business" => "design",
+:"pet supplies" => "design",
+:"baby goods/kids goods" => "design",
+:"pet services" => "design",
+# fashion
+:"spas/beauty/personal care" => "fashion",
+:"shopping/retail" => "fashion",
+:"health/beauty" => "fashion",
+:"clothing" => "fashion",
+:"jewelry/watches" => "fashion",
+# games
+  tabletop games 
+  video games
+:"athlete" => "games",
+:"games/toys" => "games",
+:"app" => "games",
+:"sports/recreation/activities" => "games",
+:"sports league" => "games",
+:"professional sports team" => "games",
+:"coach" => "games",
+:"amateur sports team" => "games",
+:"school sports team" => "games",
+:"sports venue" => "games",
+:"sport" => "games",
+# theater
+:"comedian" => "theater",
+:"arts/entertainment/nightlife" => "theater",
+:"public places" => "theater",
+:"entertainer" => "theater",
+:"public figure" => "theater",
+# photography
+:"travel/leisure" => "photography",
+:"hotel" => "photography",
+:"transit stop" => "photography",
+:"airport" => "photography",
+:"real estate" => "photography",
+:"church/religious organization" => "photography",
+:"camera/photo" => "design",
+# other
+:"organization" => "other",
+:"business services" => "other",
+:"community/government" => "other",
+:"hospital/clinic" => "other",
+:"consulting/business services" => "other",
+:"drugs" => "other",
+:"local business" => "other",
+:"lawyer" => "other",
+:"doctor" => "other",
+:"business person" => "other",
+:"politician" => "other",
+:"government official" => "other",
+:"company" => "other",
+:"product/service" => "other",
+:"bank/financial institution" => "other",
+:"insurance company" => "other"
+}
+end
+
+
+art
+  conceptual art
+  crafts
+  digital art
+  illustration
+  painting
+  performance art
+  mixed media
+  public art 
+  sculpture
+comics 
+dance
+design
+  graphic design
+  produt design
+fashion
+film & video
+  animation
+  documentary
+  narrative film 
+  short film
+  webseries
+food
+games
+  tabletop games 
+  video games
+music
+  classical music
+  country & folk 
+  electronic music
+  hip hop 
+  indie rock 
+  jazz
+  metal
+  pop 
+  rock
+  world music 
+photography
+publishing
+  art book
+  children's book
+  fiction
+  journalism 
+  nonfiction
+  perodical
+  poetry
+  radio & podcast
+technology
+  hardware
+  open software
+theater
+
