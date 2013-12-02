b6edec7dc0fd5a27c6e72d3d0ce3ebb117bb5245
Michael Polycarpou <michaelpolycarpou@gmail.com>
2013-12-01 21:45:47 -0500

    adding assets to pipeline in capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,8 +46,14 @@ role :db, "#{server_ip}", :primary => true
 
 # set :log_level, :debug
 # set :pty, true
-
-# set :linked_files, %w{config/database.yml}
+desc "tail log files"
+task :tail, :roles => :app do
+  run "tail -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
+    puts "#{channel[:host]}: #{data}"
+    break if stream == :err
+  end
+end
+#set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
