f145b8a75add13e00f3127b0af331a00a3bbc6da
Michael Polycarpou <michaelpolycarpou@gmail.com>
2013-12-01 20:55:41 -0500

    adding the files needed for deploying site to a server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,49 @@
+#set :application, "set your application name here"
+#set :repository,  "set your repository location here"
+#
+## set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+## Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+#
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+#
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
 require 'bundler/capistrano' # for bundler support
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+require 'rvm/capistrano' # for bundler support
+
+set :application, 'recommender-app'
+set :user, 'deployer' # Whatever the User You Make on Your Server
+set :branch, "deployment"
+set :repository, "git@github.com:team-middle/recommender-app.git"
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
-
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+set :scm, :git
+set :rvm_type, :system
+default_run_options[:pty] = true
 # set :format, :pretty
+set :server_ip, '162.243.246.245' # This should be your server IP
+role :web, "#{server_ip}"
+role :app, "#{server_ip}"
+role :db, "#{server_ip}", :primary => true
+
 # set :log_level, :debug
 # set :pty, true
 
@@ -17,25 +53,25 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
-end
+# namespace :deploy do
+#
+#  desc 'Restart application'
+#  task :restart do
+#    on roles(:app), in: :sequence, wait: 5 do
+#      # Your restart mechanism here, for example:
+#      # execute :touch, release_path.join('tmp/restart.txt')
+#    end
+#  end
+#
+#  after :restart, :clear_cache do
+#    on roles(:web), in: :groups, limit: 3, wait: 10 do
+#      # Here we can do anything such as:
+#      # within release_path do
+#      #   execute :rake, 'cache:clear'
+#      # end
+#    end
+#  end
+#
+#  after :finishing, 'deploy:cleanup'
+#
+#end
